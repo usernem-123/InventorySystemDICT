99c8c53b4a1e4534792501edc857f2803444e8fa
H4X <shiro101272004@gmail.com>
2026-07-30 09:38:00 +0800

    done2

--- obj/Debug/net10.0/InventorySystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/InventorySystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d1e092ba76baefd070eaf6021a95533682c91c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80cfbbf5d374292846f8deb1f66409bb11f9079a")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
